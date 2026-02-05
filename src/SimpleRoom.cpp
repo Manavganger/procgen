ee582fb71d920a62e386840e0b212a64b8fc3c8a
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2026-02-04 21:58:05 -0800

    Fix Constructor

--- src/SimpleRoom.cpp
+++ src/SimpleRoom.cpp
@@ -2,7 +2,8 @@
 
 // Constructor
 SimpleRoom::SimpleRoom(int** tiles, int numRooms, Size minSize, Size maxSize)
+    : tiles(tiles), numRooms(numRooms), minSize(minSize), maxSize(maxSize)
 {
-    tiles = tiles;
+    // leave empty 
 }
 

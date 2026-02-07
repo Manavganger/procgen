4130c154fc16402f4ef3cd45ff26da8d676e9b4a
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2026-02-06 18:21:18 -0800

    Revert main to srp

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,12 @@
 #include <SFML/Graphics.hpp>
 #include "../header/SimpleRoom.hpp"
-#include "../header/BinarySpacePartition.hpp"
 
 sf::Color valueToColorMap(int value)
 {
     switch (value)
     {
-        case 1: return sf::Color(150, 150, 150); // open space
-        default: return sf::Color(70, 70, 70);   // wall
+        case 1: return sf::Color(150, 150, 150);	//open space
+        default: return sf::Color(70, 70, 70);		//default should be a wall, not an open space
     }
 }
 
@@ -15,28 +14,27 @@ int main()
 {
     const int tileSize = 10;
     const int gap = 1;
-    const int margin = 10;
+    const int margin = 10; // can remove later
     const int cols = 120;
     const int rows = 75;
 
     const int windowWidth  = margin * 2 + cols * tileSize + (cols - 1) * gap;
     const int windowHeight = margin * 2 + rows * tileSize + (rows - 1) * gap;
 
-    sf::RenderWindow window(sf::VideoMode({windowWidth, windowHeight}), "Tile Grid");
+    sf::RenderWindow window(
+        sf::VideoMode({windowWidth, windowHeight}),
+        "Tile Grid"
+    );
 
-    int tiles[rows][cols] = {}; // zero-init
+    // for some reason you have to do this to prevent type decay
+    int tiles[rows][cols] = {}; // init to zero
     int* tilesPtr[rows];
     for (int i = 0; i < rows; ++i)
         tilesPtr[i] = tiles[i];
 
-    // procgen logic goes here
-    BinarySpacePartition bsp(tilesPtr, 10, {5,5}, {13,13}, {rows, cols});
-
-    BSPNode* root = bsp.getRoot(); 
-    bsp.split(root);
-    bsp.createRoomsBSP(root);
-    bsp.connectBSPRooms();
-    //procgen logic end
+    //procgen logic goes here
+    SimpleRoom sr(tilesPtr, 40, {5, 5}, {13, 13}, {rows, cols});
+    sr.createRooms();
 
     sf::RectangleShape tile({tileSize, tileSize});
 
@@ -48,17 +46,22 @@ int main()
                 window.close();
         }
 
-        window.clear(sf::Color(30,30,30));
+		// clear, draw, display loop
+
+        window.clear(sf::Color(30, 30, 30));
 
         for (int y = 0; y < rows; ++y)
         {
             for (int x = 0; x < cols; ++x)
             {
                 tile.setFillColor(valueToColorMap(tiles[y][x]));
+
+				// set position based on top left corner 
                 tile.setPosition({
-                    static_cast<float>(margin + x*(tileSize+gap)),
-                    static_cast<float>(margin + y*(tileSize+gap))
+                    static_cast<float>(margin + x * (tileSize + gap)),
+                    static_cast<float>(margin + y * (tileSize + gap))
                 });
+
                 window.draw(tile);
             }
         }

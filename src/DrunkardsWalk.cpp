8bac44236ad345955a8905f3e841c4a772c62d88
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2026-03-03 19:57:55 -0800

    Add auxiliary methods definitions

--- src/DrunkardsWalk.cpp
+++ src/DrunkardsWalk.cpp
@@ -1,2 +1,7 @@
-#include "../header/DrunkardsWalk"
+#include "../header/DrunkardsWalk.hpp"
+
+DrunkardsWalk:: DrunkardsWalk(GridPtr tiles, Size maxSize, int walkLen, float minCover): tiles(tiles), maxSize(maxSize), walkLen(walkLen), minCover(minCover)
+{
+    // leave empty, already initialized to walls in main.cpp
+}
 

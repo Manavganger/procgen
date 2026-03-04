5fd97ce106f87d61a735c6b5896f4da7e30c4c68
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2026-03-03 19:55:16 -0800

    Add Auxiliary Method Definitions

--- header/DrunkardsWalk.hpp
+++ header/DrunkardsWalk.hpp
@@ -6,9 +6,18 @@ class DrunkardsWalk
 {
     private:
         const Size maxSize;
-        const int walkLen;              // how many "steps" the drunkard walks
+        const int walkLen;              // how many "steps" the drunkard walks before stopping 
         const float minCover;           // what percent of the map needs to be covered before stopping 
+        int visitedCount = 0;
+
         GridPtr tiles;
+        BitArray visited;
+        std::vector<Position> visitedList;
+
+        const bool enoughGridCover(void);
+        const bool checkVisited(int r, int c);
+        const int[] getRandomTile(void);
+        void addVisited(int r, int c);
 
     public:
         DrunkardsWalk(GridPtr tiles, Size maxSize, int walkLen, float minCover);

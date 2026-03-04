df27ac7b2ab81053224af745c8185bf2656bbc1c
Manav Ganger <11431811+Manavganger@users.noreply.github.com>
2026-03-03 18:59:02 -0800

    Add Drunkards Walk Header

--- header/DrunkardsWalk.hpp
+++ header/DrunkardsWalk.hpp
@@ -4,7 +4,15 @@
 
 class DrunkardsWalk
 {
-    
+    private:
+        const Size maxSize;
+        const int walkLen;              // how many "steps" the drunkard walks
+        const float minCover;           // what percent of the map needs to be covered before stopping 
+        GridPtr tiles;
+
+    public:
+        DrunkardsWalk(GridPtr tiles, Size maxSize, int walkLen, float minCover);
+        void updateGrid(void);
 }
 
 #endif
